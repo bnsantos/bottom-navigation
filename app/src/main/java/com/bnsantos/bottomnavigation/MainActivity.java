337311af03f4475ed0b29cac42692a4e3c7347f6
Bruno Santos <brunonascimentosantos@gmail.com>
2017-01-05 15:15:40 -0200

    selecting home first

--- app/src/main/java/com/bnsantos/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/bnsantos/bottomnavigation/MainActivity.java
@@ -1,13 +1,51 @@
 package com.bnsantos.bottomnavigation;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+  private BottomNavigationView mNavigationView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+    mNavigationView.setOnNavigationItemSelectedListener(this);
+    initHome();
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_profile:
+        Toast.makeText(this, "TODO profile", Toast.LENGTH_SHORT).show();
+        break;
+      case R.id.action_home:
+        Toast.makeText(this, "TODO home", Toast.LENGTH_SHORT).show();
+        break;
+      case R.id.action_settings:
+        Toast.makeText(this, "TODO settings", Toast.LENGTH_SHORT).show();
+        break;
+    }
+    return true;
+  }
+
+  private void initHome(){
+    Menu menu = mNavigationView.getMenu();
+    for (int i = 0; i< menu.size(); i++) {
+      MenuItem menuItem = menu.getItem(i);
+      if (menuItem.getItemId() == R.id.action_home) {
+        menuItem.setChecked(true);
+        break;
+      }
+    }
   }
 }
